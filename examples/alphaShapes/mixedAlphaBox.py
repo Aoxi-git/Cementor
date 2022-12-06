51c7750066814d692fb87b0043ef522cd2c95885
bchareyre <bruno.chareyre@grenoble-inp.fr>
2022-12-06 15:06:43 +0100

    tuning example script

--- examples/alphaShapes/mixedAlphaBox.py
+++ examples/alphaShapes/mixedAlphaBox.py
@@ -1,74 +1,67 @@
 import random
-N=10
+N=30
+
+def filterPos(X):
+    # the first condition create a concavity (for fun), the other crops to extract a cylinder from a cube   
+    return (X - Vector3(N/2,N/2,N)).norm() < N/2 or (X - Vector3(N/2,X[1],N/2)).norm() > N/2
+
+random.seed(2)
 for i in range(N):
     for j in range(N):
         for k in range(N):
-            O.bodies.append(sphere((i+0.01*random.random(),j+0.01*random.random(),k+0.01*random.random()),0.49,color=(0.7,0.7,0.1)))
+            X= Vector3(i+0.01*random.random(),j+0.01*random.random(),k+0.01*random.random())
+            if not filterPos(X):
+                O.bodies.append(sphere(X,0.49,color=(0.7,0.7,0.1)))
 
-n = Vector3(1,1,1); n.normalize()
-for b in O.bodies:
-    if (b.state.pos).norm() < N/2:
-        O.bodies.erase(b.id)
-    if (b.state.pos - b.state.pos.dot(n)*n).norm()<2.5:
-        O.bodies.erase(b.id)
-        
 boxes= O.bodies.append([box((N/2,-0.51,N/2),(N,0,N),fixed=True),box((N/2,N-0.49,N/2),(N,0,N),fixed=True)])
+O.bodies[boxes[1]].state.vel=(0,-0.1,0)
 
-TW=TesselationWrapper(far=100)
+TW=TesselationWrapper()
 TW.triangulate()
 TW.addBoundingPlane(1,True)
 TW.addBoundingPlane(1,False)
-a = 2
+a = 6
 shrinkedA = (sqrt(a)-0.5)**2
+fixedA = True
 
 
-ag = TW.getAlphaGraph(alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=False)
-graph = GlExtra_AlphaGraph(tesselationWrapper=TW,wire=False)
-#graph.alpha=a
-#graph.shrinkedAlpha=shrinkedA
-#graph.fixedAlpha=False
-graph.lighting=True
+ag = TW.getAlphaGraph(alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=fixedA)
+graph = GlExtra_AlphaGraph(tesselationWrapper=TW,wire=True)
 
 from yade import qt
 rr = qt.Renderer()
 rr.extraDrawers = [graph]
 
-TW.applyAlphaForces(stress = -Matrix3.Identity, alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=False, reset = False)
-for b in O.bodies: 
-    if bool(b) and isinstance(b.shape,Sphere): b.shape.color *= O.forces.f(b.id).norm()
-    else :  b.shape.color = (1,0,0)
+TW.applyAlphaForces(stress = -Matrix3.Identity, alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=fixedA, reset = False)
 
-#from yade import qt
-#qt.Controller()
-#rr = qt.Renderer()
-#graph = GlExtra_AlphaGraph()
-#rr.extraDrawers = [graph]
-#graph.alpha=4
-#graph.shrinkedAlpha=((sqrt(graph.alpha)-0.5)**2)
-#graph.fixedAlpha=True
 
-def updateMembrane():
+def updateMembraneForces():
     TW.triangulate(reset=True)
     TW.addBoundingPlane(1,True)
     TW.addBoundingPlane(1,False)
-    TW.applyAlphaForces(stress = -1000*Matrix3.Identity, alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=False, reset = False)
-    #FIXME: suboptimal, we can't re-use the above cosntruct because it would try to insert fictious spheres again
+    TW.applyAlphaForces(stress = -1000*Matrix3.Identity, alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=fixedA, reset = False)
+    
+def displayMembrane():
+    #FIXME: suboptimal, we can't re-use the existing triangulation because it would try to insert fictious spheres again
     # as a consequence we rebuild the whole thing just for display :-/
     TW.triangulate(reset=True)
     TW.addBoundingPlane(1,True)
     TW.addBoundingPlane(1,False)
-    ag = TW.getAlphaGraph(alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=False)
+    ag = TW.getAlphaGraph(alpha=a,shrinkedAlpha=shrinkedA,fixedAlpha=fixedA)
     graph.refresh()
-
+    for b in O.bodies: 
+        if isinstance(b.shape,Sphere):
+            if O.forces.permF(b.id)!=Vector3.Zero:
+                b.shape.color = (0.8,0.4,0.4)
+            else:
+                b.shape.color = (0.4,0.7,0.4)
+        else :  b.shape.color = (1,0,0)
 
 v = qt.View()
 
-
-newton=NewtonIntegrator(damping=0.3)
-
 O.engines=[
 	ForceResetter(),
-	InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Box_Aabb()]),
+	InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Box_Aabb()],verletDist=0.3),
 	InteractionLoop(
 		[Ig2_Sphere_Sphere_ScGeom(),Ig2_Box_Sphere_ScGeom()],
 		[Ip2_FrictMat_FrictMat_FrictPhys()],
@@ -76,6 +69,14 @@ O.engines=[
 	),
 	GlobalStiffnessTimeStepper(active=1,timeStepUpdateInterval=100,timestepSafetyCoefficient=0.8),
 	#triax,
-	newton,
-	PyRunner(iterPeriod=50,command="updateMembrane()",label="membrane")
+	NewtonIntegrator(damping=0.3),
+	PyRunner(iterPeriod=50,initRun=True,command="updateMembraneForces()",label="membrane"),
+	PyRunner(iterPeriod=100,initRun=True,command="displayMembrane()",label="GuiMembrane")
 ]
+O.timingEnabled=True
+O.step()
+    
+# make Video
+#O.engines = O.engines + [qt.SnapshotEngine(fileBase=O.tmpFilename(), iterPeriod=100, label='snapshotter')]
+#def done():
+	#makeVideo(snapshotter.snapshots,out='alphaWithWalls.mpeg', fps=20, kbps=5000)
