aea351e5eee3e32e1f8ac659f407762de47b5d82
bchareyre <bruno.chareyre@grenoble-inp.fr>
2023-03-20 18:28:07 +0100

    avoid compile error on ubuntu16.04/stretch

--- lib/triangulation/KinematicLocalisationAnalyser.cpp
+++ lib/triangulation/KinematicLocalisationAnalyser.cpp
@@ -715,7 +715,7 @@ namespace CGT {
 				id = cell->vertex(i)->info().id();
 				v = v + (TS1->grain(id).sphere.point() - TS0->grain(id).sphere.point());
 			}
-		v /= Real(3);
+		v *= 0.33333333333333333333;
 		return v;
 	}
 
