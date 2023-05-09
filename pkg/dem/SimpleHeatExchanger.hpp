7466d96bbc46333dcff6efd620714ed0a126f5b5
brzezink <karol.brze@gmail.com>
2023-05-09 10:52:55 +0200

    update

--- pkg/dem/SimpleHeatExchanger.hpp
+++ pkg/dem/SimpleHeatExchanger.hpp
@@ -10,7 +10,7 @@ class SimpleHeatExchanger : public PeriodicEngine {
 private:
 	bool  needsInit; // KB: I just leave it temporarily.
 	void  init();
-	std::map<int, int> bodyIdtoPosition;// maps th body id to its position in vector
+	std::map<int, int> bodyIdtoPosition;// maps the body id to its position in vector
 	
 
 
@@ -22,13 +22,14 @@ public:
 		((vector<Real>,T,,,"T - temperature in [K]."))
 		((vector<Real>,cap,,,"Specific heat capacity [J/(kg*K)] (449 is value for granite)."))
 		((vector<Real>,cond,,,"An analog of heat conductivity but the unit is not [W/(m*K)] but [W/(m^2*K)] - need to be found by callibration."))
-		((vector<int>,bodyId,,,"Ids of bodies (actual bodies and quasiBodies). It is recommended to use negative values for quasi bodies so it is not mixed with real bodies."))
-		((vector<bool>,bodyReal,,,"If true, body is real, else is quasiBody."))
+		((vector<int>,bodyId,,,"Ids of bodies (actual bodies and dummyBodies). It is recommended to use negative values for dummy bodies so it is not mixed with real bodies."))
+		((vector<bool>,bodyReal,,,"If true, body is real, else is dummyBody."))
 		((vector<Real>,bodyEth,,,"Thermal energy of body. Note it is here for reading purposes only, but I leave it for development phase.."))
 		((vector<int>,clumpId,-1,,"ClumpId of this body twin."))
-		((vector<Real>,quasiInteractionionA,,,"Areas of interactions."))
-		((vector<Real>,quasiInteractionionId1,,,"Id1 of interactions."))
-		((vector<Real>,quasiInteractionionId2,,,"Id2 of interactions."))
+		((vector<Real>,dummyInteractionionA,,,"Areas of interactions."))
+		((vector<Real>,dummyInteractionionId1,,,"Id1 of interactions."))
+		((vector<Real>,dummyInteractionionId2,,,"Id2 of interactions."))
+		((bool,onlyDummyInteractionions,false,,"If true, the heat is exchanged only via dummy interactions."))
 
 			,//!!!!!!!! uwaga - ten przecinek dopiero po wszystkich argumentach
 			/*ctor*/ needsInit=true; //  KB: I just leave it temporarily.
